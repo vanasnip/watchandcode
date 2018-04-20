4a248361764f461cf6f2e30f118aca800196c7c6
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-20 09:26:45 +0100

    functions calling functions requirements met

--- js/app.js
+++ js/app.js
@@ -3,18 +3,29 @@
  *
  */
 
-  // *  It should have a place to store todos
-  var todos = ['item 1','item 2','item 3','item 4'];
-  // *  It should have a way to display todos
+// *  It should have a store todos
+var todos = ['item 1','item 2','item 3','item 4'];
+// *  It should have a function to display todos
+function displayTodo(){
   console.log('Ivano\'s',todos);
-  // *  it should have a way to add new todos
-  todos.push('item 5');
-  console.log('Ivano\'s',todos);
-  //*  It should have a way to change a todo
-  todos[0] = 'item one';
-  console.log('Ivano\'s',todos);
-  //*  It should have a way to delete a todo
-  todos.splice(3,1);
-  console.log('Ivano\'s',todos);
-
+}
+// *  it should have a function to add new todos
+function addNewTodo(item){
+  todos.push(item);
+  displayTodo();
+}
+//*  It should have a function to change a todo
+function changeTodo(key,item){
+  todos[key] = item; 
+displayTodo();
+}
+//*  It should have a function to delete a todo
+function deleteTodo(key){
+  todos.splice(key,1);
+displayTodo();
+}
+displayTodo();
+addNewTodo('item 5');
+changeTodo(3,'item four');
+deleteTodo(4);
 
