bf4c91d997fed8701866c91974b54da8494bf703
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-21 16:12:04 +0100

    Boolean - completed todo added

--- js/app.js
+++ js/app.js
@@ -3,32 +3,61 @@
  *
  */
 
-// *  It should store the todos array on an object
+// todotodos.addTodo should add objects
+// todotodos.changeTodo should change the todoText property
+// todotodos.toggleCompleted should change the completed property
 
-var todos = {
-  list : ['item 1','item 2','item 3','item 4'],
-// *  It should have a displayTodo method 
-displayTodo: function (){
-  console.log('Ivano\'s',this.list);
-},
-// *  it should have a addNewTodo method
-addNewTodo: function (item){
-  this.list.push(item);
-  this.displayTodo();
-},
-//*  It should have a changeTodo method
-changeTodo:function (key,item){
-  this.list[key] = item; 
-this.displayTodo();
-},
-//*  It should have a deleteTodo method 
-deleteTodo:function (key){
-  this.list.splice(key,1);
-this.displayTodo();
-}};
 
-todos.displayTodo();
-todos.addNewTodo('item 5');
-todos.changeTodo(3,'item four');
-todos.deleteTodo(4);
+// *  It should store the myToDos array on an object
 
+var myToDos = {
+  todos : [
+    {
+      todoText: 'item 1',
+      completed:false
+    },
+    {
+      todoText: 'item 2',
+      completed:false
+    },
+    {
+      todoText: 'item 3',
+      completed:false
+    },
+    {
+      todoText: 'item 4',
+      completed:false
+    }
+  ],
+  // *  It should have a displayTodo method 
+  displayTodo: function (){
+    console.log('Ivano\'s',this.todos);
+  },
+  // *  it should have a addNewTodo method
+  addNewTodo: function (todoText){
+      this.todos.push({
+        todoText : todoText,
+        completed : false
+      });
+      this.displayTodo();
+  },
+  //*  It should have a changeTodo method
+  changeTodo:function (key,item){
+    this.todos[key].todoText = item; 
+    this.displayTodo();
+  },
+  //*  It should have a deleteTodo method 
+  deleteTodo:function (key){
+    this.todos.splice(key,1);
+    this.displayTodo();
+  },
+  toggleCompleted: function(key){
+    this.todos[key].completed = !this.todos[key].completed;
+    this.displayTodo();
+  }
+};
+myToDos.displayTodo();
+myToDos.addNewTodo('item 5');
+myToDos.changeTodo(3,'item four');
+myToDos.deleteTodo(2);
+myToDos.toggleCompleted(1);
