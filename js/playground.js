2d6ecdb40009ab4ad57b437ef23fb59d5af0609f
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-24 15:11:52 +0100

    custom for each function that operates on arrays

--- js/playground.js
+++ js/playground.js
@@ -13,3 +13,19 @@ function _01(){
  function _02(){
   setTimeout(function(){console.log('wake up Ivano')}, 5000)
  }
+ function _03(){
+   var students = ['johnathan','jenny','elliot'];
+   function logname(name){
+     console.log(name);
+   }
+   students.forEach(function(student){
+     logname(student);
+   });
+
+   function custForEach(myArray,myFunction){
+     for(var i=0;i<myArray.length;i++){
+       myFunction('::::'+myArray[i]);
+     };
+   }
+   custForEach(students,logname);
+ } //_03();
