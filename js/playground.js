31291fa3d945b2f8a2931bb11df0bb5cc748d243
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-24 16:09:50 +0100

    adding event listeners

--- js/playground.js
+++ js/playground.js
@@ -10,22 +10,31 @@ function _01(){
   }
   logNumbers(getRandomNumber(100),getRandomNumber);
 }
- function _02(){
+function _02(){
   setTimeout(function(){console.log('wake up Ivano')}, 5000)
- }
- function _03(){
-   var students = ['johnathan','jenny','elliot'];
-   function logname(name){
-     console.log(name);
-   }
-   students.forEach(function(student){
-     logname(student);
-   });
+}
+function _03(){
+  var students = ['johnathan','jenny','elliot'];
+  function logname(name){
+    console.log(name);
+  }
+  students.forEach(function(student){
+    logname(student);
+  });
 
-   function custForEach(myArray,myFunction){
-     for(var i=0;i<myArray.length;i++){
-       myFunction('::::'+myArray[i]);
-     };
-   }
-   custForEach(students,logname);
- } //_03();
+  function custForEach(myArray,myFunction){
+    for(var i=0;i<myArray.length;i++){
+      myFunction('::::'+myArray[i]);
+    };
+  }
+  custForEach(students,logname);
+} //_03();
+function _04(){
+  var elems = document.getElementsByClassName('todo');
+  console.log(elems[0]);
+  if(elems.length > 0){
+    for(var i=0;i<elems.length;i++){
+      elems[i].addEventListener('click',function(e){console.log(e.srcElement.innerHTML)});
+    };
+  }
+} _04();
