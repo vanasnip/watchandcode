b7653809c5f36ddb2e9d3a5788f2b8770a224bc6
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-04-24 14:47:01 +0100

    SetTimeout

--- js/playground.js
+++ js/playground.js
@@ -1,14 +1,15 @@
-function getRandomNumber(limit){
-  return Math.floor(Math.random() * Math.floor(limit));
-}
-
-function logNumbers(n,func){
-  for (var i=0;i<n+10;i++){
-    console.log(func(i));
+function _01(){
+  // passing functions around
+  function getRandomNumber(limit){
+    return Math.floor(Math.random() * Math.floor(limit));
   }
+  function logNumbers(n,func){
+    for (var i=0;i<n+10;i++){
+      console.log(func(i));
+    }
+  }
+  logNumbers(getRandomNumber(100),getRandomNumber);
 }
-
-
-logNumbers(getRandomNumber(100),getRandomNumber);
-
-
+ function _02(){
+  setTimeout(function(){console.log('wake up Ivano')}, 5000)
+ }
